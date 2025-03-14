4565caa3296b14f228489338e4cb2824b8c2ab6b
Widly Delone <widlydelone23@gmail.com>
2025-03-14 19:16:08 -0400

    Initial commit - React frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Environmental Report Generator</h1>
+      <p>Enter environmental data below to generate a report.</p>
+    </div>
+  );
+}
+
+export default App;
